05e2f86d8892277fd0695a4b2030d10fb1f6f9bc
Adnan Karim <adnan.karim.dev@gmail.com>
2024-10-12 13:48:54 -0600

    comment update for future

--- redefeyn/src/app/home/page.tsx
+++ redefeyn/src/app/home/page.tsx
@@ -2,6 +2,9 @@
 import { useState, useEffect } from "react";
 import TypingEffect from "@/components/ui/TypingEffect";
 
+// so we will create a new component here to open home page.
+// we will get the data from the table backend_customerreviewinfo. Ideally, we will handle all the data processing in the backend.
+
 export default function DuplicateReviewPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-white space-y-4"></div>
